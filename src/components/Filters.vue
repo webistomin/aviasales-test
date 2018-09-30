bd9b311bf7c7ea5f3a2acac9d5d3e7c282bc91c1
webistomin <webistomin@gmail.com>
2018-09-30 17:47:17 +0300

    Сверстал фильтр цены

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -3,13 +3,13 @@
     <div class="filters__block">
       <span class="filters__title">Валюта</span>
       <ul class="filters__list">
-        <li class="filters__item">
-          <button class="filters__btn">Rub</button>
+        <li class="filters__tab">
+          <button class="filters__btn filters__btn--active">Rub</button>
         </li>
-        <li class="filters__item">
+        <li class="filters__tab">
           <button class="filters__btn">Usd</button>
         </li>
-        <li class="filters__item">
+        <li class="filters__tab">
           <button class="filters__btn">Eur</button>
         </li>
       </ul>
@@ -55,5 +55,80 @@
     border-radius: 5px;
     background-color: #ffffff;
     box-shadow: 0 1px 4px rgba(91, 137, 164, 0.25);
+    padding-top: 15px;
+    padding-left: 15px;
+
+    &__title {
+      display: block;
+      color: #4a4a4a;
+      font-family: "Open Sans", Arial, sans-serif;
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 19px;
+      letter-spacing: 0.5px;
+      text-transform: uppercase;
+      margin-bottom: 9px;
+    }
+
+    &__list {
+      display: flex;
+
+      &--column {
+        flex-direction: column;
+      }
+    }
+
+    &__tab {
+      width: 68px;
+      min-height: 40px;
+      border: 1px solid #d2d5d6;
+      color: #2196f3;
+      margin-left: -1px;
+
+      &:hover {
+        border-color: #64b5f5;
+        z-index: 1;
+      }
+
+      &:first-child {
+        border-radius: 5px 0 0 5px;
+      }
+
+      &:last-child {
+        border-radius: 0 5px 5px 0;
+      }
+
+    }
+
+    &__btn {
+      display: block;
+      width: 100%;
+      height: 100%;
+      font-family: "Open Sans", Arial, sans-serif;
+      font-size: 12px;
+      font-weight: 600;
+      line-height: 22px;
+      text-transform: uppercase;
+      letter-spacing: 0.5px;
+      color: #2196f3;
+
+      &:hover,
+      &:focus {
+        color: #2196f3;
+        background-color: #f2fcff;
+        border-radius: inherit;
+      }
+
+      &--active {
+        color: #ffffff;
+        background-color: #2196f3;
+      }
+    }
+
+    &__block {
+      margin-bottom: 30px;
+    }
+
+
   }
 </style>
